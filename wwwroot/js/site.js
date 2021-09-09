307fe2ed4e8574f68befe10a0d09d07d96c12b9c
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2021-09-08 18:05:45 -0700

    Overall update to aesthetics

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,2 +1,29 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+
+let i = 0;
+let text = 'Software Engineer.';
+let speed = 50;
+
+function typeWriter() {
+    if (i < text.length && document.getElementById("typewriter-effect") != null) {
+        document.getElementById("typewriter-effect").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    typeWriter();  
+});
+
+// Switching between active tab underline
+const tabs = document.querySelectorAll('.tab');
+tabs.forEach(tab => {
+    tab.addEventListener('click', function () {
+        const current = document.querySelector('.tab.active');
+        current.classList.remove('active');
+        this.classList.add('active');
+        console.log("testing");
+    })
+})
